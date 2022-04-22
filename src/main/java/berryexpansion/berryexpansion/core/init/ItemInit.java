ba0209ece9f38a3717d2068afdc863a82a76532c
vinesaucebeep <88556555+vinesaucebeep@users.noreply.github.com>
2022-04-22 11:04:36 -0400

    tried to add byg

--- src/main/java/berryexpansion/berryexpansion/core/init/ItemInit.java
+++ src/main/java/berryexpansion/berryexpansion/core/init/ItemInit.java
@@ -10,7 +10,7 @@ import net.minecraftforge.registries.RegistryObject;
 public class ItemInit {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, BerryExpansion.MOD_ID);
-    
+
     public static final RegistryObject<Item> SWEET_BERRY_JAM = ITEMS.register("sweet_berry_jam", () -> new HoneyBottleItem(new Item.Properties().tab(BerryExpansion.BE_TAB).food(new FoodProperties.Builder().nutrition(6).saturationMod(0.1F).build()).craftRemainder(Items.GLASS_BOTTLE).stacksTo(16)));
     public static final RegistryObject<Item> SWEET_BERRY_JAM_SANDWICH = ITEMS.register("sweet_berry_jam_sandwich", () -> new Item(new Item.Properties().tab(BerryExpansion.BE_TAB).food(new FoodProperties.Builder().nutrition(5).saturationMod(0.6f).build())));
     public static final RegistryObject<Item> BLACKBERRIES = ITEMS.register("blackberries", () -> new ItemNameBlockItem(BlockInit.BLACKBERRY_BUSH.get(), new Item.Properties().food(new FoodProperties.Builder().nutrition(2).saturationMod(0.1F).build()).tab(BerryExpansion.BE_TAB)));
